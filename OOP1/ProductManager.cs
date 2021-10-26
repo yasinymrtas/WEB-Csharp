9a2da1523eb964b2d35674c5872c9d167d702013
Yasin <enver@DESKTOP-GBKB46D>
2021-10-26 21:31:32 +0300

    26 Ekim Çalışma

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add()
+        {
+
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        
+
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager();
+            productManager.Topla2(6, 2);
+            productManager.Topla(6, 3);
+            int toplamSonucu = productManager.Topla(6, 3);
+            
+            //Console.WriteLine(productManager.Topla(6,3)*2);
+            Console.WriteLine(toplamSonucu*2);
+
+        }
+    }
+}
